57377d79ca70695ae5fb86ad39815a4376a31ba7
VickyE2 <f.b.cgamingco@gmail.com>
2024-08-21 15:53:44 +0100

    commit me

--- src/main/java/com/vickyeka/vickyeskorraaddon/TempestBending.java
+++
@@ -1,6 +0,0 @@
-package com.vickyeka.vickyeskorraaddon;
-
-// This is the main Tempest bending file..... There will be changes...... I think?
-
-public class TempestBending {
-}
---
+++ src/main/java/com/vickyeka/vickyeskorraaddon/VickyEsPKA.java
@@ -0,0 +1,18 @@
+package com.vickyeka.vickyeskorraaddon;
+
+import com.projectkorra.projectkorra.ProjectKorra;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VickyEsPKA extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Initialization code here
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Cleanup code here
+    }
+}
+
